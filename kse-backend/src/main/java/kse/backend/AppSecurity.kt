82bb1fd2aa3efffe7643cfcc1552cedd18fe3d35
yingzhuo <yingzhor@gmail.com>
2020-07-06 05:44:57 +0800

    2020-07-06 05:44:57

--- kse-backend/src/main/java/kse/backend/AppSecurity.kt
+++ kse-backend/src/main/java/kse/backend/AppSecurity.kt
@@ -1,22 +1,24 @@
 package kse.backend
 
-import org.springframework.context.annotation.Bean
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
-import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableWebSecurity
 open class AppSecurity : WebSecurityConfigurerAdapter() {
 
+    @Value("\${management.endpoints.web.base-path:/actuator}")
+    lateinit var actuatorPrefix: String
+
     override fun configure(http: HttpSecurity) {
         // @formatter:off
         http.authorizeRequests()
-                .antMatchers("/actuator/**")
+                .antMatchers("$actuatorPrefix/**")
                     .permitAll()
                 .anyRequest()
                     .authenticated()
@@ -29,17 +31,15 @@ open class AppSecurity : WebSecurityConfigurerAdapter() {
         // @formatter:off
         auth.inMemoryAuthentication()
                     .withUser("admin")
-                    .password("{noop}admin")
+                    .password("{MD5}21232f297a57a5a743894a0e4a801fc3")
                     .roles("ADMIN")
                 .and()
                     .withUser("yingzhuo")
-                    .password("{noop}yingzhuo")
+                    .password("{MD5}1102ea8656d577d8f73fcc9e8cf9b182\n")
                     .roles("USER")
+                .and()
+                    .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder())
         // @formatter:on
     }
-
-    @Bean
-    open fun passwordEncoder(): PasswordEncoder =
-            PasswordEncoderFactories.createDelegatingPasswordEncoder()
 
 }
--- kse-frontend/src/main/java/kse/frontend/AppFeign.kt
+++ kse-frontend/src/main/java/kse/frontend/AppFeign.kt
@@ -4,11 +4,13 @@ import com.github.yingzhuo.carnival.openfeign.EnableFeignClients
 import feign.Capability
 import feign.Contract
 import feign.RequestInterceptor
-import feign.auth.BasicAuthRequestInterceptor
+import feign.RequestTemplate
 import feign.micrometer.MicrometerCapability
 import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Component
 
 @Configuration
 @EnableFeignClients
@@ -20,8 +22,12 @@ open class AppFeign {
     @Bean
     open fun capability(registry: MeterRegistry): Capability = MicrometerCapability(registry)
 
-    @Bean
-    open fun httpBasicInterceptor(): RequestInterceptor =
-            BasicAuthRequestInterceptor("admin", "admin")
+    @Component
+    object FeignGlobalRequestInterceptor : RequestInterceptor {
+        override fun apply(template: RequestTemplate) {
+            template.header(HttpHeaders.AUTHORIZATION, "Basic YWRtaW46YWRtaW4=")
+            template.header(HttpHeaders.USER_AGENT, "OpenFeign")
+        }
+    }
 
 }
