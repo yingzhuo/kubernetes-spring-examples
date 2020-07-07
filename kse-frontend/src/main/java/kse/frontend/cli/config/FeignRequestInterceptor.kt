39f421e9c3c1117ff3e27dc88724c4f271e8f27a
yingzhuo <yingzhor@gmail.com>
2020-07-08 07:56:42 +0800

    2020-07-08 07:56:42

--- kse-frontend/src/main/java/kse/frontend/cli/config/FeignRequestInterceptor.kt
+++ kse-frontend/src/main/java/kse/frontend/cli/config/FeignRequestInterceptor.kt
@@ -11,6 +11,10 @@ object FeignRequestInterceptor : RequestInterceptor {
     override fun apply(template: RequestTemplate) {
         template.header(HttpHeaders.AUTHORIZATION, "Basic YWRtaW46YWRtaW4=") // admin:admin
         template.header(HttpHeaders.USER_AGENT, "OpenFeign")
+        template.header(HttpHeaders.ACCEPT, "application/json;charset=UTF-8")
+        template.header(HttpHeaders.ACCEPT_ENCODING, "UTF-8")
+        template.header(HttpHeaders.ACCEPT_CHARSET, "UTF-8")
+        template.header(HttpHeaders.ACCEPT_LANGUAGE, "en-US,zh-CN")
     }
 
 }
