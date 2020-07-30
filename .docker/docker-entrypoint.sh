a41608d8ad846f80d324e57196eced2e59292683
yingzhuo <yingzhor@gmail.com>
2020-07-30 18:46:54 +0800

    2020-07-30 18:46:54

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -1,6 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
+
+if [[ "${SPRING_PROFILES_ACTIVE}" == "" ]]; then
+  echo "[ERROR]: 环境变量'SPRING_PROFILES_ACTIVE'没有指定"
+  exit 1
+fi
+
+if [[ "${APP_TIMEZONE}" == "" ]]; then
+  echo "[ERROR]: 环境变量'APP_TIMEZONE'没有指定"
+  exit 1
+fi
+
+if [[ "${APP_LANG}" == "" ]]; then
+  echo "[ERROR]: 环境变量'APP_LANG'没有指定"
+  exit 1
+fi
+
+if [[ "${APP_COUNTRY}" == "" ]]; then
+  echo "[ERROR]: 环境变量'APP_COUNTRY'没有指定"
+  exit 1
+fi
 
 exec java \
   -Djava.security.egd=file:/dev/./urandom \
